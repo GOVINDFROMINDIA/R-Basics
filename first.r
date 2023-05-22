f544fd6dd316ff181ff29f72619efd280af0c06f
GO V IND <govind123.ga@gmail.com>
2023-05-22 15:54:25 +0530

    Add files via upload

---
+++ first.r
@@ -0,0 +1,23 @@
+# Define a variable
+x <- 10
+
+# Example 1: if statement
+if (x > 0) {
+  print("x is greater than 0")
+}
+
+# Example 2: if-else statement
+if (x > 0) {
+  print("x is greater than 0")
+} else {
+  print("x is less than or equal to 0")
+}
+
+# Example 3: if-else if-else statement
+if (x > 0) {
+  print("x is greater than 0")
+} else if (x < 0) {
+  print("x is less than 0")
+} else {
+  print("x is equal to 0")
+}
