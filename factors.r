00c3ba699e1e7772891b61e06e943ffa8f34c3f4
GO V IND <govind123.ga@gmail.com>
2023-05-22 23:37:30 +0530

    Add files via upload

---
+++ factors.r
@@ -0,0 +1,10 @@
+data <- c("False","True","False","False","False","True","False","True","True","True")
+factor_data <- factor(data)
+print(factor_data)
+print(is.factor(factor_data))
+
+height = c(140, 155, 168, 142, 163, 149, 130)
+weight = c(50, 51, 68, 55, 69, 54, 42)
+gender = c("female", "male", "female", "male", "male", "female", "male")
+input_data <- data.frame(height,weight,gender)
+print(input_data)
