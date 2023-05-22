42fccd000d95ee26b1cd34679fbcee476b6c4174
GO V IND <govind123.ga@gmail.com>
2023-05-22 20:04:47 +0530

    Update functions.r

--- functions.r
+++ functions.r
@@ -1,7 +1,6 @@
 print_sequence <- function(start, end) {
   if (start <= end) {
-    seq_numbers <- start:end
-    print(seq_numbers)
+    print(start:end)
   } else {
     print("Invalid input: Start number should be less than or equal to End number.")
   }
